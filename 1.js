bb1fa7ce48d953ea0c8e7e6e8651df12d0de8ccb
duongngt <cctm05d005@gmail.com>
2018-09-11 21:32:47 +0700

    upload resume

---
+++ 1.js
@@ -0,0 +1,27 @@
+﻿ $(function(){
+          $('.button,.ab').click(function() {
+          	$('body,html').animate({scrollTop:$('#boxabout').offset().top},500);
+          	return false;
+          });
+          $('.sk').click(function() {
+          	$('body,html').animate({scrollTop:$('#boxskills').offset().top},700);
+          	return false;
+          });
+          $('.exe').click(function() {
+          	$('body,html').animate({scrollTop:$('#boxexpedu').offset().top},900);
+          	return false;
+          });
+          $('.bl').click(function() {
+          	$('body,html').animate({scrollTop:$('#boxblog').offset().top},1000);
+          	return false;
+          });
+          $('.ct').click(function() {
+          	$('body,html').animate({scrollTop:$('#boxcontact').offset().top},1000);
+          	return false;
+          });
+          $('.bnthome').click(function() {
+          	$('body,html').animate({scrollTop:0},700);
+          	return false;
+          });
+})  ;
+ 
